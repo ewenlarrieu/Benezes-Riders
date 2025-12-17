bf1ff52795fd30452acb50d4b2bb87c825a15fd8
ewenlarrieu <ewenlarrieu200515@gmail.com>
2025-12-17 10:07:09 +0100

    Fix: correction casse fichier authRoutes

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoutes from "./routes/AuthRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import albumRoutes from "./routes/AlbumRoute.js";
 
 dotenv.config();
